563c96f13d276346a347a7072c67cde3bd9c27f3
Furkan Yazar <furkanyazar9853@gmail.com>
2022-05-01 16:56:14 +0300

    Update some a tags

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', component: ProductComponent },
   { path: 'products', component: ProductComponent },
   { path: 'products/category/:categoryId', component: ProductComponent }
 ];
