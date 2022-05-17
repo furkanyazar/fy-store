d3d58eab04b13de9f557ce803269b90b25327d00
Furkan Yazar <furkanyazar9853@gmail.com>
2022-05-17 16:55:12 +0300

    add login component

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,42 @@
+import { ToastrService } from 'ngx-toastr';
+import { LoginModel } from './../../models/loginModel';
+import { AuthService } from './../../services/auth.service';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from "@angular/forms";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm: FormGroup
+
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private toastrService: ToastrService) { }
+
+  ngOnInit(): void {
+    this.createLoginForm()
+  }
+
+  createLoginForm(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  login(): void {
+    if (this.loginForm.valid) {
+      let loginModel: LoginModel = Object.assign({}, this.loginForm.value)
+
+      this.authService.login(loginModel).subscribe(response => {
+        this.toastrService.success(response.message)
+        localStorage.setItem('token', response.data.token)
+      }, responseError => {
+        this.toastrService.error(responseError.error)
+      })
+    }
+  }
+
+}
